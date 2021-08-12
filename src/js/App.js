5fdf83234d4d8b4f07c451750e368f5d269c5f9b
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-12 19:42:34 +0800

    Added Footer component

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from './components/Card'
+import Footer from './components/Footer'
 
 const App = () => {
     return (
-        <div className='App'>
-            <Card />
+        <div className='app'>
+            <div>
+                <Card />
+                <Footer />
+            </div>
         </div>
     )
 }
