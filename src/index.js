c701849fdd62a9d62a1ef9ea399aa14990bceb66
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-12 17:59:38 +0800

    Restructured App component files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const HelloWorld = () => {
-    return <h1>Hello World</h1>
-}
-
-const el = document.querySelector('#app');
-ReactDOM.render(<HelloWorld />, el);
