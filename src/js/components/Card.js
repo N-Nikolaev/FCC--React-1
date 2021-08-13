dbe24a7e70184a5656f5ee2eae8cf1d0cc1a1540
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2021-08-13 17:27:33 +0800

    Added API fetch function to Card component

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+const useFetch = url => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setData(data);
+                setLoading(false);
+            });
+    }, []);
+
+    return { data, loading };
+}
 
 const Card = () => {
+    const url = 'https://api.quotable.io/random';
+    const { data, loading } = useFetch(url);
+    
+    console.log( data );
+
     return (
-        <main className="card">
-            <h1>Random Quote Machine</h1>
+        <main className="card" id="quote-box">
+            { loading && <div>Loading</div> }
+            { 
+                data && 
+                <blockquote>
+                    <p>{data.content}</p>
+                    <cite>{data.author}</cite>
+                </blockquote> 
+            }
         </main>
     )
 }
